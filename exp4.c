e0310ab519275ea94091f71f6ad70ab7c0c05c2f
Kakarot35 <mhatrekaran989@gmail.com>
2026-06-17 22:00:19 +0530

    Feat: even or odd

---
+++ exp4.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main(){
+    int num;
+        printf("Enter any Number:");
+        scanf("%d",&num);
+
+        (num%2==0)?
+        printf("The given number is Even number"):
+        printf("The given number is odd number");
+
+        return 0 ;
+}
