2be844353b7393e7c017e256267ad3ed3f2d7644
Kakarot35 <mhatrekaran989@gmail.com>
2026-06-19 22:31:37 +0530

    Feat: Loops

---
+++ loop.c
@@ -0,0 +1,34 @@
+# include <stdio.h>
+
+ 
+int main(){
+    int n;
+     char ch;
+
+        printf("Welcome to series\n");
+
+        printf("Enter your n terms :");
+        scanf("%c", &n);
+
+            for ( char ch= 'a'; ch <= 'z'; ch++)
+            {
+              
+                printf("%c\n",ch);
+            }
+            int i=0;
+           ///while (i<=n)
+           // {
+             //   printf("%d\n",i);
+             //  i++;
+        //  }
+           // do
+          //  {
+         //       printf("%d\n",i);
+         ///       i++;
+          //  } while (i<=n);
+            
+            
+        return 0;
+
+}
+   
